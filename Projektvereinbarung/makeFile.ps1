cf8de4bdcc39714d2e138153f3c25dbf60be666b
Jurij Maïkoff <sergejurij.maikoff@students.fhnw.ch>
2019-10-01 15:43:00 +0200

    moving content

---
+++ Projektvereinbarung/makeFile.ps1
@@ -0,0 +1,19 @@
+$dir = Get-Location 
+set-Location $dir
+$input = Get-Item src\*.md | Resolve-Path -Relative
+
+
+
+pandoc $input -o out/Projektvereinbarung.pdf `
+    --metadata-file=metadata.yml `
+    --from markdown `
+    --template eisvogel `
+    --listings `
+    --number-sections `
+    -V lang=de `
+    -V papersize:a4 `
+    --toc `
+    --toc-depth=2 `
+    --bibliography=biblio.bib  `
+    --csl=ieee.csl `
+    --highlight-style breezedark
---
+++ Projektvereinbarung/src/010-termine.md
@@ -0,0 +1,14 @@
+# Termine
+
+| Datum  |Event   |   
+|:---|:---|
+| 24.09.2019  | Kickoff  |
+| NOV         | Erster Konzept Entwurf + MVP Testnetzwerk  |
+| 28.11.2019  | Zwischenpräsentation  |
+| DEZ         | erste Konzept Version  |
+| FEB         | Testnetzwerk fertig    |
+| 20.03.2019  | Abgabe Bachelorthesis  |
+| 13. - 24.04.2019  | Verteidigung  |
+
+
+
